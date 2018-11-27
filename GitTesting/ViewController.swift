28c6cb5b8e1f3ed7186c9f1f1fd1ca5cace9e8a6
Tripat Khetarpaul <tripatkhetarpaul@apple-wifi.local>
2018-11-27 10:36:24 +0530

    initial commit for first git project my first view controller

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		print("asif")
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
